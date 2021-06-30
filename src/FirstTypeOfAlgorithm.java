1ff4583f36389ab0be33d158d5fe3c0747dca83b
Conterry <glebois05@gmail.com>
2021-06-30 23:27:13 +0300

    Initial commit

---
+++ src/FirstTypeOfAlgorithm.java
@@ -0,0 +1,6 @@
+public class FirstTypeOfAlgorithm {
+    //simple algorithm where every third enumerator will be fizz and fifth will be buzz or "fizz buzz" every 15
+    public static void main(String[] args) {
+        System.out.println("Fizz Buzz");
+    }
+}
